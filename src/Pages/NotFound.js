9a46ed09709175c486a494b2e02ba4be9a05ef0e
Ariful145 <arifulh145@gmail.com>
2021-10-19 23:37:42 +0600

    healthcare-0.0.7 ed 10

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -13,16 +13,9 @@ function NotFound() {
           <p className="mb-8">
             But dont worry, you can find plenty of other things on our homepage.
           </p>
-
-          <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
-            back to homepage
-          </button>
         </div>
         <div className="max-w-lg">
-          <img
-            src="https://imagevars.gulfnews.com/2017/8/28/1_16a083ca8dc.2081539_2008253998_16a083ca8dc_medium.jpg"
-            alt=""
-          />
+          <img src="https://i.ibb.co/SRjPZH0/3828537.jpg" alt="" />
         </div>
       </div>
     </div>
